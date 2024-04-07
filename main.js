959b53bc0bbcb9640f78225caa2b5f88ae991b8f
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-07 19:59:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+// String Concatenation
+let firstName = "Mubashir";
+let lastName = "Khan";
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+let favFood = "Briyani";
+let favFastFood = "Pizza";
+let favMeal = favFood + ' and ' + favFastFood;
+console.log(favMeal);
+let myName = "Mubashir";
+let friendsName = "Daniyal";
+let friendship = myName + ' ' + friendsName;
+console.log(friendship);
+console.log(`${myName} and ${friendsName} are friends.`);
---
+++ main.ts
@@ -0,0 +1,16 @@
+// String Concatenation
+let firstName: string = "Mubashir";
+let lastName: string = "Khan";
+let fullName: string = firstName + ' ' + lastName;
+console.log(fullName);
+
+let favFood: string = "Briyani";
+let favFastFood: string = "Pizza";
+let favMeal: string = favFood + ' and ' + favFastFood;
+console.log(favMeal);
+
+let myName = "Mubashir";
+let friendsName = "Daniyal";
+let friendship = myName + ' ' + friendsName;
+console.log(friendship);
+console.log(`${myName} and ${friendsName} are friends.`);
